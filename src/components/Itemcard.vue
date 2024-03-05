30bec1e818fa51fdbdfbb7378be5f26b3b33f433
Aaron <y.chen-19@student.utwente.nl>
2024-03-05 21:35:44 +0100

    fix display issues with smaller screen sizes

--- src/components/Itemcard.vue
+++ src/components/Itemcard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-5 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-102 hover:shadow-glow border-ut-pink">
     <div class="hover:shadow-lg">
-      <img class="h-48 w-full object-cover" src="https://upload.wikimedia.org/wikipedia/commons/5/51/Computer_Science_Word_Cloud.png">
+      <img class="h-36 w-full object-cover" src="https://upload.wikimedia.org/wikipedia/commons/5/51/Computer_Science_Word_Cloud.png">
     </div>
     <div class="p-8">
       <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">M11 Design Project Module (2023-2A)</div>
--- src/pages/Courses.vue
+++ src/pages/Courses.vue
@@ -1,20 +1,19 @@
 <script setup>
 import Sidebar from "@/components/Sidebar.vue";
-import Card from "@/components/Itemcard.vue";
 import Itemcard from "@/components/Itemcard.vue";
 </script>
 
 <template>
   <div class="w-screen h-screen flex bg-gray-200">
-    <!--    sidebar -->
+    <!-- sidebar -->
     <Sidebar></Sidebar>
     <!--  main -->
-    <div class="grid grid-cols-1 grid-rows-16 w-full relative">
-      <div>
+    <div class="grid grid-cols-1 grid-rows-16 relative h-screen">
+      <div class="h-[100px]">
         <h1 class="text-3xl font-arial px-10 font-bold mt-10">Courses</h1>
       </div>
-      <div class="mx-10 row-span-10">
-        <el-row class="my-10" :gutter="20">
+      <div class="mx-10 h-[calc(100vh-150px)]">
+        <el-row :gutter="20">
           <el-col :span="6">
             <Itemcard></Itemcard>
           </el-col>
@@ -28,7 +27,7 @@ import Itemcard from "@/components/Itemcard.vue";
             <Itemcard></Itemcard>
           </el-col>
         </el-row>
-        <el-row class="my-10" :gutter="20">
+        <el-row :gutter="20">
           <el-col :span="6">
             <Itemcard></Itemcard>
           </el-col>
@@ -43,7 +42,7 @@ import Itemcard from "@/components/Itemcard.vue";
           </el-col>
         </el-row>
       </div>
-      <div class="row-span-4 bg-ut-pink items-center justify-center flex text-ut-white font-arial font-bold">
+      <div class="bg-ut-pink items-center justify-center flex text-ut-white font-arial font-bold h-[50px]">
         <h3>WORK IN PROGRESS...</h3>
       </div>
     </div>
