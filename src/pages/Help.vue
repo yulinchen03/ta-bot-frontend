5995b63cd2cce867b935c8209d98c7be3e60ee57
Yulin <y.chen-19@student.utwente.nl>
2024-03-24 21:50:45 +0100

    Implemented help pages and fixed help center layouts

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,18 +1,87 @@
 <template>
-<div class="absolute inset-x-0 inset-y-0">
-  <h1 class="font-arial font-bold">This is where you will find specific help on topic {{helpid}}</h1>
-  <div class="absolute bottom-0 bg-ut-pink w-full h-12 flex justify-center items-center text-white font-bold"><h2>Work in progress...</h2></div>
-</div>
+  <div class="w-screen h-screen flex bg-gray-200">
+    <!-- sidebar -->
+    <Sidebar></Sidebar>
+    <!--  main -->
+    <div class="relative h-screen w-screen overflow-y-scroll">
+      <div class="grid grid-cols-1">
+        <Header :title="pageTitle"></Header>
+        <p class="mx-20 text-justify mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+          optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+          obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+          nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+          tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+          quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+          sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+          recusandae alias error harum maxime adipisci amet laborum. Perspiciatis
+          minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit
+          quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur
+          fugiat, temporibus enim commodi iusto libero magni deleniti quod quam
+          consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+          doloremque. Quaerat provident commodi consectetur veniam similique ad
+          earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo
+          fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore
+          suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+          modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam
+          totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam
+          quasi aliquam eligendi, placeat qui corporis!</p>
+        <div class="flex justify-center items-center">
+          <img class='object-scale-down w-2/3 rounded-md'
+               src="https://1348661504.rsc.cdn77.org/.uc/fc5bbfb1b0102841f0c0000fe710364f87eed6c6b751c00/3.jpg">
+        </div>
+        <p class="mx-20 text-justify mt-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+          optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+          obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+          nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+          tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+          quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+          sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+          recusandae alias error harum maxime adipisci amet laborum. Perspiciatis
+          minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit
+          quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur
+          fugiat, temporibus enim commodi iusto libero magni deleniti quod quam
+          consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+          doloremque. Quaerat provident commodi consectetur veniam similique ad
+          earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo
+          fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore
+          suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+          modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam
+          totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam
+          quasi aliquam eligendi, placeat qui corporis!</p>
+        <div class="flex justify-center mt-10">
+          <el-image
+              style="height: 50%"
+              :src="url"
+              :zoom-rate="1.2"
+              :max-scale="2"
+              :min-scale="0.5"
+              :preview-src-list="srcList"
+              :initial-index="0"
+              fit="contain"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
+import Sidebar from "@/components/Sidebar.vue";
+
 export default {
+  components: {Sidebar, Header},
   created() {
     this.fetchData();
   },
   data() {
     return {
-      helpid: -1
+      helpid: -1,
+      pageTitle: 'Lorem ipsum dolor'
     }
   },
   watch: {
@@ -25,4 +94,14 @@ export default {
     },
   }
 }
+</script>
+
+<script setup>
+const url =
+    'https://image.isu.pub/210305085437-97200a61da6e877cd7ffc0f9b8c52298/jpg/page_1.jpg'
+const srcList = [
+  'https://image.isu.pub/210305085437-97200a61da6e877cd7ffc0f9b8c52298/jpg/page_1.jpg',
+  'https://1348661504.rsc.cdn77.org/.uc/ibae0edb301027c3314008f110e034c4df9a1829c941d0701c3dc05000080/campus-2.jpg',
+  'https://www.utwente.nl/.uc/icdb0f7340102efb61600780a2c0375ec7e528a46ae380701c45604ac0180/7g5rhwk-dmu-e9tcfmgo2w.jpg',
+]
 </script>
--- src/pages/HelpCenter.vue
+++ src/pages/HelpCenter.vue
@@ -13,17 +13,20 @@ import Header from "@/components/Header.vue";
            style="background-image: url('https://www.utwente.nl/.uc/i6483d1530102352aaf00637dc00170c187b52a95d5e50701c3dc05000080/utimg13327.jpg');">
         <Header :title="pageTitle" class="text-gray-200 italic font-semibold"></Header>
         <div class="w-[20px] flex justify-center items-center">
-          <el-button size="large" class="custom-button absolute inset-x-0 -bottom-5 mx-auto px-4 py-2 max-w-48 font-arial">Contact Us</el-button>
+          <el-button size="large" class="custom-button absolute inset-x-0 -bottom-5 mx-auto px-4 py-2 max-w-48 font-arial"><a href = "mailto: support@ta-bot.utwente.nl">Contact Us</a>
+          </el-button>
         </div>
       </div>
-      <div class="grid grid-cols-3 gap-5 mx-20 h-[100vh-100px]">
-        <div @click="openHelp(topic.id)" class="m-4 h-[25vh] bg-white shadow-lg rounded-lg overflow-hidden p-4 max-w-md md:max-w-2xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-102 hover:shadow-glow border-ut-pink" v-for="topic in topics" :key="topic.id">
+      <div class="grid grid-cols-3 gap-3 mx-20 h-[100vh-100px]">
+        <div @click="openHelp(topic.id)" class="m-4 h-[25vh] bg-white shadow-xl rounded-xl overflow-y-auto p-4 max-w-md md:max-w-2xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-102 hover:shadow-glow border-ut-pink" v-for="topic in topics" :key="topic.id">
           <div class="flex items-center mb-4">
-            <div class="bg-ut-pink text-white rounded-full w-8 h-8 flex items-center justify-center mr-4">
+            <div class="bg-ut-pink text-white rounded-full w-8 h-8 flex items-center justify-center">
               <el-icon size="30"><QuestionFilled /></el-icon>
             </div>
+            <el-divider direction="vertical" />
             <h2 class="font-bold text-lg">{{topics[topic.id-1].title}}</h2>
           </div>
+          <el-divider />
           <p class="text-gray-700">{{topics[topic.id-1].description}}</p>
         </div>
       </div>
@@ -44,27 +47,35 @@ export default {
       topics: [{
         id: 1,
         title: 'help 1',
-        description: 'this is the first help article'
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n' +
+            '          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n' +
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n' +
+            '          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n'
       },{
         id: 2,
         title: 'help 2',
-        description: 'this is the second help article'
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n' +
+            '          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n'
       },{
         id: 3,
         title: 'help 3',
-        description: 'this is the third help article'
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n' +
+            '          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n'
       },{
         id: 4,
         title: 'help 4',
-        description: 'this is the fourth help article'
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n' +
+            '          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n'
       },{
         id: 5,
         title: 'help 5',
-        description: 'this is the fifth help article'
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n' +
+            '          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n'
       },{
         id: 6,
         title: 'help 6',
-        description: 'this is the sixth help article'
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n' +
+            '          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n'
       },
       ]
     }
