011fb25b916ce9b2ee9f44159b631ec4071a0763
JacobTheChurchEducation <j.m.kosciolek@student.utwente.nl>
2024-03-28 19:16:10 +0100

    Authentication

---
+++ src/stores/user.js
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia'
+import authService from "@/services/authService.js";
+
+export default defineStore('user', {
+    state: () => ({
+        token: '',
+        user: null,
+    }),
+    actions: {
+        async login(email, password) {
+            try {
+                const res = await authService.login(email, password)
+                console.log(res)
+                console.log(res.headers)
+
+                this.token = res.headers.authorization.split(' ')[1]
+                this.user = { email: res.data.data.email, name: res.data.data.username, role: res.data.data.role}
+
+                console.log('token:', this.token)
+                console.log('user:', this.user)
+
+            }
+            catch(err) {
+                //TODO handle error
+                console.log(err)
+            }
+        },
+        logout() {
+            //   TODO
+        },
+    }
+})
