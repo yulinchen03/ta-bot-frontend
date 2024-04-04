3a9d8513b6b17c9d4f8f12a46514b190eb984114
sserkanakin <s.akin@student.utwente.nl>
2024-04-04 14:48:02 +0200

    courses changes, differences for student and teacher added. need to add join course for student

--- src/components/Itemcard.vue
+++ src/components/Itemcard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-md bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-4 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-102 hover:shadow-glow border-ut-pink">
-    <el-button @click="this.$emit('deleteCourse')" class="absolute right-1 top-1 z-50">
+    <el-button v-if="isTeacher" @click="this.$emit('deleteCourse')" class="absolute right-1 top-1 z-50">
       <el-icon><Delete /></el-icon>
     </el-button>
     <div @click="emitClickEvent">
@@ -16,6 +16,9 @@
 </template>
 
 <script>
+import { mapStores} from "pinia";
+import useUserStore from "@/stores/user.js";
+
 export default {
   props: ['courseData'],
   emits: ['courseSelected', 'deleteCourse'],
@@ -27,6 +30,7 @@ export default {
       courseTitle: '',
       code: '',
       join_code: '',
+      isTeacher: null,
     }
   },
   methods: {
@@ -37,8 +41,16 @@ export default {
     },
     emitClickEvent() {
       this.$emit('courseSelected');
-    }
+    },
   },
+  computed: {
+      ...mapStores(useUserStore)
+    },
+  created() {
+    this.isTeacher = this.userStore.user.role === 'teacher';
+    this.isTeacher = false;
+    console.log(this.isTeacher)
+  }
 }
 </script>
 
--- src/pages/Courses.vue
+++ src/pages/Courses.vue
@@ -13,7 +13,7 @@ import Header from "@/components/Header.vue";
       <Header :title="pageTitle"></Header>
       <div class="mx-20 overflow-y-auto w-[calc(100vw-200px)] h-[calc(100vh-100px)]">
         <!-- Dynamic rows -->
-        <div v-for="(row, rowIndex) in coursesRows" :key="rowIndex" class="flex justify-start">
+        <div v-if="isTeacher" v-for="(row, rowIndex) in coursesRows" :key="rowIndex" class="flex justify-start">
           <Itemcard
               v-for="item in row"
               :key="item.id"
@@ -23,9 +23,18 @@ import Header from "@/components/Header.vue";
               @deleteCourse="deleteCourse(item.id)"
           />
         </div>
+        <div v-else>
+          <Itemcard
+              v-for="course in courses"
+              :key="course.id"
+              :courseData="course"
+              class="w-[calc(25vw-100px)]"
+              @courseSelected="openCourse(course.id)"
+          />
+        </div>
       </div>
     </div>
-    <el-dialog v-model="dialogFormVisible" title="Create Course" width="500">
+    <el-dialog v-if="isTeacher" v-model="dialogFormVisible" title="Create Course" width="500">
       <el-form :model="courseForm">
         <el-form-item label="Course Name:" class="text-black">
           <el-input v-model="courseForm.name" autocomplete="off" />
@@ -42,7 +51,7 @@ import Header from "@/components/Header.vue";
     </el-dialog>
 
 
-    <el-button size="large" @click="dialogFormVisible = true" circle class="fixed right-10 bottom-10 z-20">
+    <el-button v-if="isTeacher" size="large" @click="dialogFormVisible = true" circle class="fixed right-10 bottom-10 z-20">
       <el-icon><Plus /></el-icon>
     </el-button>
   </div>
@@ -146,6 +155,8 @@ export default {
     this.getCourses()
 
     this.isTeacher = this.userStore.user.role === 'teacher';
+    this.isTeacher = false;
+    console.log(this.isTeacher)
   }
 }
 </script>
