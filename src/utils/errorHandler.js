3b53d969a553db09f707aec02d7982e8ed527baf
JacobTheChurchEducation <j.m.kosciolek@student.utwente.nl>
2024-04-16 19:54:56 +0200

    Changing error handling

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -16,7 +16,7 @@ export default function errorHandler(error) {
     // }
 
     return ElMessage({
-        message: error?.response?.data?.status?.message || error.response.data,
+        message: error?.response?.data?.status?.errors.join(' ') || error?.response?.data?.status?.message || error.response.data ,
         type: 'error'
     })
 }
