b1d1f774834b41e8a935b255f0c04ab123ac672d
JacobTheChurchEducation <j.m.kosciolek@student.utwente.nl>
2024-04-16 13:19:58 +0200

    Delete Remember me and Forgot password?

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -38,15 +38,6 @@
                   </el-form-item>
                 </el-form>
                 <div class="flex items-center justify-between">
-                  <div class="flex items-start">
-                    <div class="flex items-center h-5">
-                      <input id="remember" v-model="loginForm.rememberMe" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800">
-                    </div>
-                    <div class="ml-3 text-sm">
-                      <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                    </div>
-                  </div>
-                  <a @click="goToResetPassword" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                 </div>
                 <el-button @click="login" class="custom-button w-3/4 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</el-button>
                 <p class="text-sm font-light text-gray-500 dark:text-gray-400">
@@ -136,9 +127,6 @@ import errorHandler from "@/utils/errorHandler.js";
       },
       goToSignup(){
         this.$router.push('/signup');
-      },
-      goToResetPassword(){
-        this.$router.push('/reset')
       }
     }
   }
